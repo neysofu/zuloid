55603ab4bbf77c4f0c15898040d4a9e457b76f20
Filippo Costa <filippocosta.italy@gmail.com>
2018-07-15 13:37:42 +0200

    ER, TD... almost

--- src/training/main.py
+++ src/training/main.py
@@ -4,115 +4,50 @@
 import chess
 import chess.pgn
 import numpy as np
-import scipy
 import tensorflow as tf
 import keras as ks
 import sqlite3
+import scipy
 import random as rnd
 import logging
 import sys
 import argparse
 import uuid
 import os
+#import numba as nb
+from chess import pgn
 from os.path import expanduser
-# For A3C (TODO)
-from multiprocessing import Pool
+from multiprocessing import Pool # For A3C (TODO)
+from scipy import spatial
 
-SEED = 42
-
-HOT_BOARD_TENSOR_WIDTH = 456
+SEED = 146
+HOT_BOARD_TENSOR_WIDTH = 455
 BOARD_TENSOR_WIDTH = 512
-MOVE_TENSOR_WIDTH = 7
-
-AVERAGE_NUM_OF_MOVES_IN_CHESS_GAME = 42
+MOVE_TENSOR_WIDTH = 5
+AVERAGE_NUM_OF_MOVES_IN_CHESS_GAME = 54
 MAX_NUM_OF_REVERSIBLE_MOVES_BEFORE_DRAW = 50
-
 RATIO_OF_CHESS_960_GAMES_DURING_TRAINING = 0.4
 EXPLORATION_RATE_DURING_TRAINING = 0.08
 DISCOUNT_VALUE = 0.96
-
 LOGGING_FORMAT = "%(asctime)s @ '%(name)s.%(funcName)s:%(lineno)d' [%(levelname)s]: %(message)s"
 LOGGING_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
-
 BOARD_TO_MOVE_FILENAME = "Board2move.h5"
 BOARD_INCR_TO_BOARD_FILENAME = "BoardIncr2Board.h5"
 HOT_TO_BOARD_FILENAME = "Hot2board.h5"
 
-__version__ = "0.1"
-
-
-class Db:
-
-    INIT_TABLES_CMDS = [
-        """
-        CREATE TABLE IF NOT EXISTS games (
-            SessionId VARCHAR,
-            Date TIMESTAMP,
-            Episode INT,
-            Moves VARCHAR,
-            Result CHAR(7),
-            Ending VARCHAR)
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS positions (
-            Fen CHAR(88),
-            Event BIGINT)
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS sessions (
-            SessionId VARCHAR,
-            StartTime DATETIME,
-            EndTIme DATETIME,
-            Version VARCHAR)
-        """]
-
-    def __init__(self):
-        self.path = "{}/db.sqlite3".format(ARGS.output_dir)
-        self.connection = sqlite3.connect(self.path)
-        for cmd in Db.INIT_TABLES_CMDS:
-            self.connection.cursor().execute(cmd)
-        self.connection.commit()
-
-    def insert_game(self, game, num_episode):
-        self.connection.cursor().execute("""
-            INSERT INTO games (
-                SessionId,
-                Date,
-                Episode,
-                Moves,
-                Result
-            ) VALUES (
-                '{}',
-                '{}',
-                {},
-                '{}',
-                '{}'
-            )""".format(SESSION_ID,
-                        "Now",
-                        1337,
-                        game.accept(
-                            chess.pgn.StringExporter(
-                                headers=False,
-                                variations=False,
-                                comments=False)),
-                        numeric_result(game.end().board())))
-        self.connection.commit()
-
-    def insert_session(self, session_id):
-        self.connection.cursor().execute("""
-        INSERT INTO sessions (
-            SessionId,
-            Version
-        ) VALUES ("{}", "{}")""".format(session_id, __version__))
-        self.connection.commit()
+__author__ = "Filippo Costa @neysofu"
+__version__ = "0.2"
 
 
 class Board(chess.Board):
+    """
+    A custom subclass of 'chess.Board' to support some non-standard operations.
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def to_tensor(self, resoluteness_factor):
+    def to_tensor(self):
         return np.array([[
             bit
             for bitmask in [
@@ -124,12 +59,45 @@ class Board(chess.Board):
             self.has_kingside_castling_rights(chess.WHITE),
             self.has_kingside_castling_rights(chess.BLACK),
             self.has_queenside_castling_rights(chess.WHITE),
-            self.has_queenside_castling_rights(
-                chess.BLACK), resoluteness_factor,
+            self.has_queenside_castling_rights(chess.BLACK),
             min(self.halfmove_clock, MAX_NUM_OF_REVERSIBLE_MOVES_BEFORE_DRAW) /
             MAX_NUM_OF_REVERSIBLE_MOVES_BEFORE_DRAW, self.fullmove_number /
             (self.fullmove_number + AVERAGE_NUM_OF_MOVES_IN_CHESS_GAME)
         ]])
+
+    def stochastic_push(self, q_values):
+        """
+        Explores the board accoring to some Q values over a subset of the legal
+        moves. In its simplest form, it is implemented as a form of
+        epsilon-greedy exploration, but can be tuned for better performance.
+        Returns the chosen move.
+        """
+        # TODO: exploration.
+        return q_values[max(q_values.keys())]
+
+    def adjust_move_tensor(self, move_tensor):
+        def move_tensor_diff(tensor_1, tensor_2):
+            """
+            Compute the difference between two move tensors.
+            """
+            source_diff = scipy.spatial.distance.euclidean(tensor_1[:2],
+                                                           tensor_2[:2])
+            target_diff = scipy.spatial.distance.euclidean(tensor_1[2:],
+                                                           tensor_2[2:])
+            return source_diff ** 2 + target_diff
+        tensor_1 = [x * 8 for x in move_tensor[0][:-1]]
+        legal_moves = list(self.legal_moves)
+        min_diff = 139 # Maximum diff between two moves.
+        for i, legal_move in enumerate(legal_moves):
+            tensor_2 = [chess.square_file(legal_move.from_square),
+                        chess.square_rank(legal_move.from_square),
+                        chess.square_file(legal_move.to_square),
+                        chess.square_rank(legal_move.to_square)]
+            diff = move_tensor_diff(tensor_1, tensor_2)
+            if diff < min_diff:
+                min_diff = diff
+                move_i = i
+        return (legal_moves[move_i], min_diff / 139)
 
 def numeric_result(board):
     result = board.result()
@@ -152,46 +120,6 @@ def piece_to_tensor(piece):
         ], piece.piece_type == chess.KING, piece.color, not piece.color
     ]
 
-class Action:
-
-    def __init__(self, tensor):
-        self.tensor = tensor
-        self.source_file = int(tensor[0][0] * 8)
-        self.source_rank = int(tensor[0][1] * 8)
-        self.target_file = int(tensor[0][2] * 8)
-        self.target_rank = int(tensor[0][3] * 8)
-        self.source = chess.square(self.source_file, self.source_rank)
-        self.target = chess.square(self.target_file, self.target_rank)
-        self.move = chess.Move(self.source, self.target)
-
-    def __sub__(self, move_2):
-        source_file_1 = chess.square_file(self.move.from_square)
-        source_rank_1 = chess.square_file(self.move.to_square)
-        target_file_1 = chess.square_rank(self.move.from_square)
-        target_rank_1 = chess.square_rank(self.move.to_square)
-        source_file_2 = chess.square_file(move_2.from_square)
-        source_rank_2 = chess.square_file(move_2.to_square)
-        target_file_2 = chess.square_rank(move_2.from_square)
-        target_rank_2 = chess.square_rank(move_2.to_square)
-        return scipy.spatial.distance.cosine(
-            [source_file_1, source_rank_1, target_file_1, target_file_1],
-            [source_file_2, source_rank_2, target_file_2, target_file_2])
-
-    def fit_to(self, board):
-        if self.move in board.legal_moves:
-            return self.move
-        max_moves_similarity = 0
-        index_of_candidate_move = 0
-        i = 0
-        for legal_move in board.legal_moves:
-            moves_similarity = self - legal_move
-            if moves_similarity > max_moves_similarity:
-                max_moves_similarity = moves_similarity
-                index_of_candidate_move = i
-            i += 1
-        return list(board.legal_moves)[index_of_candidate_move]
-
-
 class Agent:
 
     # Hyperparameters. Boy, isn't ML fun...
@@ -200,104 +128,125 @@ class Agent:
     DISCOUNT_VALUE = 1.0
     BATCH_SIZE = 512
     # All hyperparameter values decay over time to guarantee convergence.
-    DECAY_RATE = 0.99998
+    DECAY_RATE = 2E-5
 
-    def __init__(self, board):
+    def __init__(self, database):
+        self.db = database
+        self.db_i = 0
         self.model = Agent.playing_model()
-        #self.read_from_disk()
-        self.board = Board()
+        self.read_from_disk_if_exists()
         self.batch = []
         self.learning_rate = Agent.LEARNING_RATE
         self.exploration_rate = Agent.EXPLORATION_RATE
         self.discount_value = Agent.DISCOUNT_VALUE
         # TODO: hyperparameter values decay.
         num_episodes = int(ARGS.num_episodes)
-        self.i = 0
-        while self.i < num_episodes:
-            self.play()
-            self.i += 1
-            if self.i % 500 == 0:
-                LOGGER.info("Num. of episodes is {}".format(self.i))
+        self.episode_i = 0
+        while self.episode_i < num_episodes:
+            self.play(setup_board())
+            self.episode_i += 1
         self.write_to_disk()
 
-    def play(self):
+    def play(self, board):
+        """
+        Test the agent at a game of chess against itselt and register what
+        happens. These information will then be used to train the model.
+        """
+        # FIXME: currently ignored.
         resoluteness_factor = np.random.random()
-        LOGGER.info(
-            "New game (game ID is '{}'). Starting position: \"{}\"".format(
-                SESSION_ID, self.board.fen()))
+        LOGGER.debug(f"New game (episode n.{self.episode_i + 1}) with "
+                    f"initial position '{board.fen()}', "
+                    f"resoluteness factor ~{round(resoluteness_factor, 3)}.")
         i = 0
-        while not self.board.is_game_over():
-            board_tensor = self.board.to_tensor(resoluteness_factor)
-            move = self.move(board_tensor)
-            self.board.push(move)
-            LOGGER.info(f"{i}. {move.uci()} ({self.model.predict(board_tensor)[0][4]})")
+        predictions = []
+        while not board.is_game_over():
+            input_tensor = board.to_tensor()
+            output_tensor = self.model.predict(input_tensor)
+            predictions.append((input_tensor, output_tensor))
+            # FIXME
+            move = board.adjust_move_tensor(output_tensor)[0]
+            board.push(move)
             i += 1
-        DB.insert_game(chess.pgn.Game.from_board(self.board), self.i)
-        LOGGER.info("Game ended. Result: {}. Num. of moves: {}".format(
-            self.board.result(), self.board.fullmove_number))
-        self.backpropagate_state_value()
-        self.board = Board()
-
-    def move(self, board_tensor):
-        if np.random.random() < Agent.EXPLORATION_RATE:
-            return rnd.choice(list(self.board.legal_moves))
-        else:
-            return Action(self.model.predict(board_tensor)).fit_to(self.board)
+        LOGGER.debug(f"{i} moves were played. The result is "
+                     f"{board.result()}.")
+        self.queue_for_training(chess.pgn.Game.from_board(board), predictions)
 
     def playing_model():
         model = ks.models.Sequential()
         model.add(
             ks.layers.Dense(
                 HOT_BOARD_TENSOR_WIDTH,
-                activation='relu',
+                activation="linear",
                 input_dim=HOT_BOARD_TENSOR_WIDTH))
-        model.add(ks.layers.Dropout(0.5))
-        model.add(ks.layers.Dense(64, activation='relu'))
-        model.add(ks.layers.Dropout(0.5))
-        model.add(ks.layers.Dense(MOVE_TENSOR_WIDTH, activation='sigmoid'))
-        model.compile(
-            loss='categorical_crossentropy',
-            optimizer="sgd",
-            metrics=['accuracy'])
+        model.add(ks.layers.Dropout(0.2))
+        model.add(ks.layers.LeakyReLU())
+        model.add(ks.layers.Dense(64, activation="linear"))
+        model.add(ks.layers.LeakyReLU())
+        model.add(ks.layers.Dense(MOVE_TENSOR_WIDTH, activation="sigmoid"))
+        model.compile(loss="categorical_crossentropy",
+                      optimizer=ks.optimizers.Adamax(lr=Agent.LEARNING_RATE,
+                                                        decay=Agent.DECAY_RATE),
+                      metrics=["accuracy"])
         return model
 
-    def backpropagate_state_value(self):
-        game = chess.pgn.Game.from_board(self.board)
-        state_value = numeric_result(self.board)
-        stack_of_boards = [game.board()]
-        for move in game.main_line():
-            stack_of_boards.append(stack_of_boards[-1])
-            stack_of_boards[-1].push(move)
-        stack_of_boards = [Board(fen=board.fen()) for board in stack_of_boards][::-1]
-        for board in stack_of_boards:
-            board_tensor = self.board.to_tensor(0.1337)
-            target_tensor = self.model.predict(board_tensor)
-            target_tensor[0][4] += 0.01 * (state_value - target_tensor[0][4])
-            state_value = target_tensor[0][4]
-            self.model.fit(board_tensor, target_tensor)
+    def queue_for_training(self, game, predictions):
+        """
+        Train the model based on a game played by the AI. Its inputs along with
+        the relative outputs are included to avoid duplicate computation.
+        """
+        state_values = backpropagate_state_value([p[1][0][4] for p in predictions],
+                                                 game)
+        for i, prediction in enumerate(predictions):
+            prediction[1][0][4] = state_values[i]
+        target_predictions = predictions
+        self.batch.append(target_predictions)
+        if len(self.batch) >= Agent.BATCH_SIZE:
+            self.model.fit([x[0] for x in self.batch],
+                           [x[1] for x in self.batch])
+
+    def read_from_disk_if_exists(self):
+        try:
+            self.model.load_weights(ARGS.output_dir + "/model.h5")
+            LOGGER.info("Successfully loaded weights from the last session.")
+        except:
+            LOGGER.error("The weights of the model couldn't be found. New "
+                         "weights will be initialized.")
 
     def write_to_disk(self):
         self.model.save(ARGS.output_dir + "/model.h5")
 
-    def read_from_disk(self):
-        self.model.load_weights(ARGS.output_dir + "/model.h5")
+def setup_board():
+    chess960 = rnd.uniform(0, 1) < RATIO_OF_CHESS_960_GAMES_DURING_TRAINING
+    return Board(chess960=chess960)
 
-    def remember(self):
-        # FIXME: experience replay is still a long way to go before being
-        # usable.
-        self.batch.append((board, move))
+def proximal_policy_optimization_loss(actual_value, predicted_value, old_prediction):
+    advantage = actual_value - predicted_value
+    def loss(y_true, y_pred):
+        prob = K.sum(y_true * y_pred)
+        old_prob = K.sum(y_true * old_prediction)
+        r = prob/(old_prob + 1e-10)
+        return -K.log(prob + 1e-10) * K.mean(K.minimum(r * advantage, K.clip(r, min_value=0.8, max_value=1.2) * advantage))
+    return loss
 
-    def setup_board(self):
-        chess960 = rnd.uniform(0, 1) < RATIO_OF_CHESS_960_GAMES_DURING_TRAINING
-        self.board = Board(chess960=chess960)
-
-def cli():
-    parser = argparse.ArgumentParser()
-    default_output_dir = expanduser("~") + "/Z64C-training-target/"
-    parser.add_argument("-o", "--output-dir", default=default_output_dir)
-    parser.add_argument("-n", "--num-episodes", default="1024")
-    return parser
-
+def backpropagate_state_value(state_values, game):
+    """
+    Backpropagates the state values estimated during a game of chess according
+    the TD(lambda) algorithm.
+    See http://www.incompleteideas.net/book/ebook/node75.html for details.
+    """
+    # TODO: find a custom fitting curve specific to chess. See
+    # https://lichess.org/insights/LeelaChess/acpl/material
+    result = game.end().board().result()
+    eligibility_traces = np.zeros(len(state_values))
+    trace_decay_parameter = 0.01
+    discount_rate = 0.96
+    weight = 0.82
+    for i in range(len(state_values)):
+        eligibility_traces *= trace_decay_parameter * discount_rate
+        eligibility_traces[i] += 1
+        #error = result + state_values[i+1] - state_values[i]
+        #state_values += alpha * error * eligibility_traces
+    return state_values
 
 def init_logger(log_file_path):
     logger = logging.getLogger()
@@ -314,30 +263,35 @@ def init_logger(log_file_path):
     logger.addHandler(file_handler)
     return logger
 
+def cli():
+    parser = argparse.ArgumentParser()
+    default_output_dir = expanduser("~") + "/Z64C-training-output/"
+    parser.add_argument("-o",
+                        "--output-dir",
+                        default=default_output_dir)
+    parser.add_argument("-n",
+                        "--num-episodes",
+                        type=int,
+                        default="1")
+    return parser
 
 def main():
     global ARGS
-    global DB
     global LOGGER
     global SESSION_ID
     tf.logging.set_verbosity(tf.logging.ERROR)
     ARGS = cli().parse_args(sys.argv[1:])
     os.makedirs(ARGS.output_dir, exist_ok=True)
-    DB = Db()
     LOGGER = init_logger("{}/session.log".format(ARGS.output_dir))
     SESSION_ID = uuid.uuid1()
     print("")
-    LOGGER.info("Starting a new Z64C training session with ID '{}'.".format(
-        SESSION_ID))
-    DB.insert_session(SESSION_ID)
+    LOGGER.info(f"New training session with ID {SESSION_ID}.")
     tf.set_random_seed(SEED)
     np.random.seed(SEED)
     rnd.seed(SEED)
     tf.reset_default_graph()
     Agent(Board())
-    LOGGER.info(
-        "Exiting the Z64C training session with ID '{}'.".format(SESSION_ID))
-
+    LOGGER.info("Goodbye.")
 
 def exit_gracefully():
     # TODO
@@ -348,6 +302,7 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        LOGGER.info("Keyboard interrupt received. Exiting gracefully. Press"
-                    "CTRL+C a second time to exit immediately.")
+        LOGGER.info("Keyboard interrupt received. Exiting gracefully. Press "
+                    "CTRL+C a second time to exit immediately and disregard any"
+                    " changes.")
         exit_gracefully()
