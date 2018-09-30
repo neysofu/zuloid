b163a31ead8feb4c3bdc28bc8240d1070c879885
Filippo Costa <filippocosta.italy@gmail.com>
2018-09-30 11:35:07 +0200

    Remove target/ directory

--- target/Release/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/pippoc/Z64C
-# Build directory: /Users/pippoc/Z64C/target/Release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Z64C "/Users/pippoc/Z64C/target/Release/Z64C_TEST")
