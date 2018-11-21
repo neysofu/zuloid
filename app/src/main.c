396c52651cf5794945d89c1a4d25b980f65992a2
Filippo Costa <filippocosta.italy@gmail.com>
2018-11-21 23:25:31 +0100

    Add comments to main.c

--- app/src/main.c
+++ app/src/main.c
@@ -2,6 +2,9 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * @file main.c
+ * @brief Provides an entry point to the program.
  */
 
 #include "bitboards.h"
@@ -11,16 +14,23 @@
 #include "utils.h"
 #include <stdio.h>
 #include <sys/select.h>
-#include <sysexits.h>
 
 int
 main(void)
 {
+	/**
+	 * The communication protocol over stdin/stdout is line-oriented, so we
+	 * make sure that line buffering is on.
+	 */
 	setlinebuf(stdin);
 	setlinebuf(stdout);
-	setlinebuf(stderr);
 	TRACE("Line buffering set for all three standard channels.\n");
+	/** Initialize bitboard lookup tables. */
 	bb_init();
+	/**
+	 * Let's make cJSON use xmalloc too for better consistency in case of
+	 * out-of-memory errors.
+	 */
 	struct cJSON_Hooks cjson_hooks = { xmalloc, free };
 	cJSON_InitHooks(&cjson_hooks);
 	struct Engine *engine = engine_new();
