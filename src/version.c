a3bc96341638b4a9e21f7f9b60506df25f05b05e
Filippo Costa <filippocosta.italy@gmail.com>
2019-02-06 15:44:30 +0100

    Bring back version info to globals.c

--- src/version.c
+++
@@ -1,8 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-#include "globals.h"
-
-const char *const Z64C_VERSION =
-  Z64C_VERSION_MAJOR "." Z64C_VERSION_MINOR "." Z64C_VERSION_PATCH;
