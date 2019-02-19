6c3bb2293e14b36727467be4f8c55998511700c2
Filippo Costa <filippocosta.italy@gmail.com>
2019-02-19 08:49:30 +0100

    Implement fast_range in separate file

--- include/fast_range.h
+++ include/fast_range.h
@@ -1,43 +1,13 @@
-/* Fair maps to intervals without division.
- * Reference: http://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
- *
- * (c) Daniel Lemire
- * Apache License 2.0
- *
- * Modifications by Filippo Costa. Original file at
- * https://github.com/lemire/fastrange/blob/master/fastrange.h. */
-
 #ifndef Z64C_FAST_RANGE_H
 #define Z64C_FAST_RANGE_H
 
-#include "utils.h"
-#include <iso646.h>
-#include <limits.h>
-#include <stddef.h>
 #include <stdint.h>
-
-#if defined(_MSC_VER) && defined(_WIN64)
-#include <intrin.h>
-#pragma intrinsic(_umul128)
-#endif
+#include <stdlib.h>
 
 static size_t
-fast_range_32(uint_fast32_t x, size_t range)
-{
-	return ((uint_fast64_t)(x) * (uint_fast64_t)(range)) >> 32;
-}
+fast_range_32(uint_fast32_t x, size_t range);
 
 static size_t
-fast_range_64(uint_fast64_t x, size_t range)
-{
-#ifdef __SIZEOF_INT128__
-	return ((__uint128_t)(x) * (__uint128_t)(range)) >> 64;
-#elif defined(_MSC_VER) && defined(_WIN64)
-	uint_fast64_t foo;
-	return _umul128(x, range, &foo);
-#else
-	return x % range;
-#endif
-}
+fast_range_64(uint_fast64_t x, size_t range);
 
 #endif
