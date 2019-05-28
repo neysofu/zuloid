03560071c4adb7a7dc58f1251e98bb60de4c134a
Filippo Costa <filippocosta.italy@gmail.com>
2019-05-28 21:36:32 +0200

    Add some comments to strtok_whitespace

--- src/utils/strtok_whitespace.c
+++ src/utils/strtok_whitespace.c
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* strtok is abominable and I hate it with the passion of a thousand burning
+ * suns. That said, we are using it in a controlled setting (i.e. a small
+ * executable) and I believe it's fine. */
 char *
 strtok_whitespace(char *str)
 {
