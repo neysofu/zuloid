02597ec4ffce1722aae501dac915bc50aa1f1c27
Filippo Costa <filippocosta.italy@gmail.com>
2020-07-10 05:12:53 +0200

    Fix minor test issues

--- test/main.c
+++ test/main.c
@@ -1,33 +1,20 @@
 #include "Unity/src/unity.h"
 
-extern void
-test_attacks(void);
-extern void
-test_cache_single_key_retrieval(void);
-extern void
-test_castling_mask(void);
-extern void
-test_char_to_file(void);
-extern void
-test_char_to_piece(void);
-extern void
-test_color(void);
-extern void
-test_fen_conversion(void);
-extern void
-test_fen_init(void);
-extern void
-test_file_to_char(void);
-extern void
-test_jsonrpc_compliance(void);
-extern void
-test_piece_to_char(void);
-extern void
-test_position_is_illegal(void);
-extern void
-test_position_is_legal(void);
-extern void
-test_rating(void);
+// clang-format off
+extern void test_attacks(void);
+extern void test_cache_single_key_retrieval(void);
+extern void test_castling_mask(void);
+extern void test_char_to_file(void);
+extern void test_char_to_piece(void);
+extern void test_color_other(void);
+extern void test_fen_conversion(void);
+extern void test_fen_init(void);
+extern void test_file_to_char(void);
+extern void test_piece_to_char(void);
+extern void test_position_is_illegal(void);
+extern void test_position_is_legal(void);
+extern void test_rating(void);
+// clang-format on
 
 int
 main(void)
@@ -38,11 +25,10 @@ main(void)
 	RUN_TEST(test_castling_mask);
 	RUN_TEST(test_char_to_file);
 	RUN_TEST(test_char_to_piece);
-	RUN_TEST(test_color);
+	RUN_TEST(test_color_other);
 	RUN_TEST(test_fen_conversion);
 	RUN_TEST(test_fen_init);
 	RUN_TEST(test_file_to_char);
-	RUN_TEST(test_jsonrpc_compliance);
 	RUN_TEST(test_piece_to_char);
 	RUN_TEST(test_position_is_illegal);
 	RUN_TEST(test_position_is_legal);
