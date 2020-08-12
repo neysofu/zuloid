eb9a2e51976141c096099396fbf6b27d452d0688
Filippo Costa <filippocosta.italy@gmail.com>
2020-08-12 07:46:58 +0200

    Add support for CLI options

--- src/main.c
+++ src/main.c
@@ -8,15 +8,18 @@
 struct Engine engine;
 
 int
-main(void)
+main(int argc, char **argv)
 {
 	init_subsystems();
+	engine_init(&engine);
 	// The number sign ensures minimal possibility of accidental evaluation by
 	// the client.
 	printf("# Zuloid %s (%s)\n", ZULOID_VERSION_VERBOSE, ZULOID_BUILD_DATE);
 	printf("# Copyright (c) 2018-2020 Filippo Costa\n");
 	printf("# Process ID: %d\n", p_process_get_current_pid());
-	engine_init(&engine);
+	for (int i = 1; i < argc && engine.status != STATUS_EXIT; i++) {
+		engine.protocol(&engine, argv[i]);
+	}
 	while (engine.status != STATUS_EXIT) {
 		char *line = read_line(stdin);
 		engine.protocol(&engine, line);
