71587ea69df43fbabfe86e7ca014cb608dd63b06
Filippo Costa <filippocosta.italy@gmail.com>
2020-08-12 08:39:50 +0200

    Remove unused globals

--- include/globals.h
+++ include/globals.h
@@ -7,12 +7,8 @@
  * details. */
 extern const int ADDRESS_SIZE;
 
-extern const char *const ZULOID_COPYRIGHT;
-extern const char *const ZULOID_LICENSE;
 /* Compiler build timestamp in YYYY-MM-DD format. */
 extern const char *const ZULOID_BUILD_DATE;
-extern const char *const ZULOID_URL;
-extern const char *const ZULOID_BACKEND_NAME;
 extern const char *const ZULOID_VERSION;
 extern const char *const ZULOID_VERSION_VERBOSE;
 
--- src/globals.c
+++ src/globals.c
@@ -4,9 +4,6 @@
 
 const int ADDRESS_SIZE = sizeof(void *) * CHAR_BIT;
 
-const char *const ZULOID_LICENSE = "MPL-2.0";
 const char *const ZULOID_BUILD_DATE = PROJECT_BUILD_DATE;
-const char *const ZULOID_URL = "https://github.com/neysofu/zuloid";
-const char *const ZULOID_BACKEND_NAME = "CPU";
 const char *const ZULOID_VERSION = PROJECT_VERSION;
 const char *const ZULOID_VERSION_VERBOSE = PROJECT_VERSION;
