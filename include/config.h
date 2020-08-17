df4c13d39d47df8c07f2c155d4c2fd1fae67b8d7
Filippo Costa <filippocosta.italy@gmail.com>
2020-08-17 19:35:18 +0200

    Remove unused includes

--- include/config.h
+++ include/config.h
@@ -1,13 +1,8 @@
 #ifndef ZULOID_CONFIG_H
 #define ZULOID_CONFIG_H
 
-#include "agent.h"
-#include "cache/cache.h"
-#include "chess/position.h"
-#include "chess/termination.h"
-#include "eval.h"
-#include "time/game_clock.h"
-#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 struct Config
 {
