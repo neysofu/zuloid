70f9f3fb005a22e8a04b75a3d9331b343872c574
Filippo Costa <filippocosta.italy@gmail.com>
2020-09-27 16:42:51 +0200

    Attempt at amalgamation

---
+++ tools/amalgamate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+find src/ -name '*.c' -exec cat {} + > zuloid.c
+gcc -std=c99 -I include/ -I lib/ -I lib/plibsys/src/ -I build/ -Lbuild/ -l:libplibsys.so zuloid.c -o zuloid
+
+#cat include/**/*.h | grep -v "^#include \"" > zuloid.c
+
+#echo -i include/ $() amalgamate.c
